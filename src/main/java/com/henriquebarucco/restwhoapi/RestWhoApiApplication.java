10ae8fbcb633dfe65de2a909d49549eabcd2a431
Henrique Barucco <henrique_barucco@hotmail.com>
2022-08-09 20:40:54 -0300

    First commit

---
+++ src/main/java/com/henriquebarucco/restwhoapi/RestWhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.henriquebarucco.restwhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWhoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWhoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henriquebarucco/restwhoapi/RestWhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquebarucco.restwhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWhoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
